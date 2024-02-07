b3b880f8181518cd2e9a32aee047dec90d7042aa
Hongvyo <wiswkd36@gmail.com>
2024-02-08 06:04:53 +0900

    added layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { theme, Layout, Menu, Breadcrumb, ConfigProvider } from 'antd';
+import { Header, Content, Footer } from 'antd/es/layout/layout';
+const { darkAlgorithm, compactAlgorithm } = theme;
 
-function App() {
+
+
+
+const App: React.FC = () => {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ConfigProvider
+        theme={{
+            "token": {
+              "colorPrimary": "#f9b17a",
+              "colorInfo": "#f9b17a",
+              "colorLink": "#f9b17a",
+              "colorBgBase": "#161e30",
+              "colorBgContainer": "#2d3250",
+              "borderRadius": 0
+            },
+            "algorithm": darkAlgorithm
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout style={{height: "100vh"}}>
+      <Header
+        style={{
+          position: 'sticky',
+          top: 0,
+          zIndex: 1,
+          width: '100%',
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <div className="demo-logo" />
+        <Menu
+        //   theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+        />
+      </Header>
+      <Content >
+        
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        Ant Design ©{new Date().getFullYear()} Created by Ant UED
+      </Footer>
+    </Layout>
+    </ConfigProvider>
+    
   );
-}
+};
 
 export default App;
