c07b3d0ba157f6d8abbede93689b9e6aa88a4991
Hongvyo <wiswkd36@gmail.com>
2024-07-02 21:31:20 +0900

    move to nextjs ]

--- src/App.Footer.tsx
+++
@@ -1,8 +0,0 @@
-import { Footer } from "antd/es/layout/layout"
-
-export const AppFooter = () => {
-    return (
-        <Footer style={{ textAlign: 'center', height: 80 }}>
-        </Footer>
-    )
-}
