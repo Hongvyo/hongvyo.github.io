eb5507d9c07de7a1d2a6b7dafb4e910f4aad3026
Hongvyo <wiswkd36@gmail.com>
2024-07-02 21:39:16 +0900

    test basepath

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  //   /**
-  //    * Set base path. This is the slug of your GitHub repository.
-  //    *
-  //    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-  //    */
-  //   basePath: "/",
+  /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/",
 
   /**
    * Disable server-based image optimization. Next.js does not support
